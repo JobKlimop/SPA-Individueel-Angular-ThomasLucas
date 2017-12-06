74ef7a379e3065bdc24a1c4324ca43221b824514
ThomasL <thomas_641991@hotmail.com>
2017-12-06 14:59:25 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  loadedFeature = 'Home';
+  imagePath: string;
+
+  constructor() {
+    this.imagePath = '../../assets/img/DSC_0216.jpg';
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
